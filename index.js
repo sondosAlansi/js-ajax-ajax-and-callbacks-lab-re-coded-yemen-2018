e9029cf232f5a89cd999485ea243fbe33fbcc972
sondos alansi <sondos.alansi@yahoo.com>
2018-10-21 19:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 $(document).ready(function (){
+  
+ $('a').click(function(){
+   var search=$('#searchTerms').val();
+     $.get('https://api.github.com/users/'+search+'/repos', function(response) {
+      
+    let re= `<ul>
+    ${response.map(r => 
+    
+     `<li>
+     <span> ${r.name} </span>
+    
+       <span>${r.owner.login}</span>
+       <img src=${r.owner.avatar_url}/>
+       <span><a href="">showCommits</a></span>
+        
+    </li>
+    
+    `)
+    .join('')}</ul>`;
+        $('#results').html(re);
+        // console.log(response);
+    });
+  
+   
+ });
+    
+    
 });

fa0c000e7fd2e179a39aecc1d4d60ebe2d4eda82
sondos alansi <sondos.alansi@yahoo.com>
2018-10-21 20:15:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 $(document).ready(function (){
+  
+ $('#ser').click(function(){
+   var search=$('#searchTerms').val();
+     $.get('https://api.github.com/users/'+search+'/repos', function(response) {
+      
+    let re= `<ul>
+    ${response.map(r => 
+    
+     `<li>
+     <a href=${r.html_url}> ${r.name} </a>
+    
+       <span>${r.owner.login}</span>
+        <span><a href="" data-re=${r.name} onclick="showCommits ()">showCommits</a></span>
+       <img src=${r.owner.avatar_url}/>
+      
+        
+    </li>
+    
+    `)
+    .join('')}</ul>`;
+        $('#results').html(re);
+       
+        // console.log(response);
+    }).fail(function(error) {
+   
+    $('#errors').html("I\'m sorry, there\'s been an error");
+   
+   
+ });
+   
+});
+
 });
